0660afb5534550f5457bd284c99eabbb46466bc5
Assis Zang <assis.compasso@dotzmkt.com.br>
2022-05-28 19:17:21 -0300

    feat: Project creation

---
+++ FPExampleApp/Helper.cs
@@ -0,0 +1,13 @@
+﻿namespace FPExampleApp
+{
+    public static class Helper
+    {
+        public static IEnumerable<T> MyWhere<T>
+            (this IEnumerable<T> source, Func<T, bool> predicate)
+        {
+            foreach (T s in source)
+                if (predicate(s))
+                    yield return s;
+        }
+    }
+}
